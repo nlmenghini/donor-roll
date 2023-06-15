126ada02cabd2e7454fdc92a9bae5cb248625247
Nick Menghini <nick.menghini@gmail.com>
2023-06-15 16:07:12 -0400

    Fixed some math added some styles
    Fixed the calculation total on the donations. Added in theming styles and the ability to set columns. Center aligned text inside columns and stacked everything.

--- donor-roll-script.js
+++ donor-roll-script.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
     var endpoint = "https://extralife.donordrive.com/api/participants/" + participantId + "/donations";
     var pageSize = 100; // Number of donations to retrieve per page
-    var totalDonationCount = 0; // Variable to hold the total donation count
     var donationIds = []; // Array to store donation IDs and check for duplicates
 
-    if (isDarkMode) {
-        $("body").addClass("dark-mode");
-    }
+    if (theme === "DarkMode") {
+        var cssFile = "dark-mode.css"; // Replace with the filename of your dark mode CSS file
+        var linkElement = $("<link rel='stylesheet' type='text/css' href='styles/" + cssFile + "'>");
+        $("head").append(linkElement);
+    } else if (theme === "LightMode") {
+        var cssFile = "light-mode.css"; // Replace with the filename of your light mode CSS file
+        var linkElement = $("<link rel='stylesheet' type='text/css' href='styles/" + cssFile + "'>");
+        $("head").append(linkElement);
+    } else if (theme === "Blue1") {
+        var cssFile = "blue-1.css"; // Replace with the filename of your Blue1 CSS file
+        var linkElement = $("<link rel='stylesheet' type='text/css' href='styles/" + cssFile + "'>");
+        $("head").append(linkElement);
+    } else if (theme === "Blue2") {
+        var cssFile = "blue-2.css"; // Replace with the filename of your Blue2 CSS file
+        var linkElement = $("<link rel='stylesheet' type='text/css' href='styles/" + cssFile + "'>");
+        $("head").append(linkElement);
+    } else if (theme === "Green") {
+        var cssFile = "green.css"; // Replace with the filename of your Green CSS file
+        var linkElement = $("<link rel='stylesheet' type='text/css' href='styles/" + cssFile + "'>");
+        $("head").append(linkElement);
+    }    
     
     // Function to retrieve the donations with pagination
     function retrieveDonations(page) {
@@ -45,19 +62,16 @@ $(document).ready(function() {
                         var donationItem = $("<li></li>");
                         
                         // Display the displayName in bold and right-align the amount
-                        
                         var displayName = donation.displayName || "Mysterious Donor";
                         var displayNameAmount = $("<div class='donor-details'></div>")
-                            .append($("<span class='donor-name'></span>").text(displayName).css("font-size", customDonorNameFontSize))
-                            .append($("<span class='dono-amount'></span>").text("$" + donation.amount).css("font-size", customDonoAmountFontSize));
+                            .append($("<span class='dono-amount'></span>").append($("<span class='dollar-symbol'></span>").text("$")).append(donation.amount).css("font-size", customDonoAmountFontSize))
+                            .append($("<span class='donor-name'></span>").text(displayName).css("font-size", customDonorNameFontSize));
                         var messageElement = null;
-                        if (showMessageElement) {
-                            messageElement = $("<div class='dono-message'></div>")
-                                .text(donation.message).css("font-size", customDonoMessageFontSize);
+                        if (showMessageElement && donation.message) {
+                            messageElement = $("<div class='dono-message'></div>").text(donation.message).css("font-size", customDonoMessageFontSize);
                         }
                         if (showDate) {
-                            var dateElement = $("<div class='dono-date'></div>")
-                                .text(formattedDate).css("font-size", customDonoDateFontSize);
+                            var dateElement = $("<div class='dono-date'></div>").text(formattedDate).css("font-size", customDonoDateFontSize);
                         }
 
                         var donationID = donation.donationID;
@@ -73,10 +87,8 @@ $(document).ready(function() {
                     }
                 });
 
-                // Update the total count of donation items
-                totalDonationCount += donations.length;
                 if (enableDonationCount) {
-                    $("#donationCount").text("Total Donations: " + totalDonationCount);
+                    $("#donationCount").text("Total Donations: " + donationIds.length);
                 }
                 
                 // Check if there are more donations to retrieve
@@ -84,7 +96,7 @@ $(document).ready(function() {
                     retrieveDonations(page + 1); // Fetch the next page
                 } else {
                     // Calculate the scrolling duration based on the number of donation items. The additional 10 seconds is to account for the header and thank you message
-                    var scrollingDuration = (scrollDurationMultiplier * totalDonationCount) + 10000;
+                    var scrollingDuration = ((scrollDurationMultiplier * donationIds.length) + 10000) / columnCount;
                     var scrollingDurationMinutes = Math.floor(scrollingDuration / 60000); // Convert duration to minutes
                     var scrollingDurationSeconds = Math.floor((scrollingDuration % 60000) / 1000); // Convert remaining duration to seconds
 
--- donor-roll-settings.js
+++ donor-roll-settings.js
@@ -1,17 +1,20 @@
 // Display settings
-var participantId = "ENTER YOUR ID"; // Enter your participant ID, you can find it at the end of the url for your Extra Life page
+var participantId = "Enter your ID"; // Enter your participant ID, you can find it at the end of the url for your Extra Life page
 var showDate = true; // Variable to control the visibility of the date element
 var showMessageElement = true; // Control the visibility of messageElement
 var scrollDurationMultiplier = 1000; // Default is 1 second per donation (1000); increase this for more time
 var customHeaderText = "Donors"; // Default "Donors"
 var customThankYou = "So long, and thanks for all the fish."; // Default "So long, and thanks for all the fish."
-var isDarkMode = true; // Default is true. Set to false for dark text.
+var columnCount = 3; //Default is 1. If you have lots of donos try 2 or 3. Would not recommend more than 3. Scroll speed will remaing the same.
+
+//Theming. Default is Blue 1. Make sure to only have one of these on.
+var theme = "Blue1"; // Options: "Blue1", "Blue2", "Green", "LightMode", "DarkMode"
 
 // Custom type sizes. A bigger number means bigger text. A smaller number, smaller text. Don't forget the px.
 var customHeaderFontSize = "100px"; // Default "100px"
 var customThankYouFontSize = "50px"; // Default "50px"
-var customDonorNameFontSize = "25px"; // Default "25px"
-var customDonoAmountFontSize = "20px"; // Default "20px"
+var customDonoAmountFontSize = "40px"; // Default "40px"
+var customDonorNameFontSize = "30px"; // Default "30px"
 var customDonoMessageFontSize = "18px"; // Default "18px"
 var customDonoDateFontSize = "16px"; // Default "16px"
 
