7b1527d0b90d9185bfca8ed5b40b53d24ce83263
Nick Menghini <nick.menghini@gmail.com>
2023-06-16 17:56:35 -0400

    fixing my defaults...

--- donor-roll-settings.js
+++ donor-roll-settings.js
@@ -6,8 +6,8 @@ var scrollDurationMultiplier = 1000; // Default is 1 second per donation (1000);
 var customHeaderText = "Donors"; // Default "Donors"
 var customThankYou = "So long, and thanks for all the fish."; // Default "So long, and thanks for all the fish."
 var columnCount = 1; //Default is 1. If you have lots of donos try 2 or 3. Would not recommend more than 3. Scroll speed will remaing the same.
-var showTotal = true; // Default is false; Turns total raised at the end on and off.
-var showLogo = true; // Default is true; Turns logo at the end on and off.
+var showTotal = false; // Default is false; Turns total raised at the end on and off.
+var showLogo = false; // Default is true; Turns logo at the end on and off.
 var scrollDelay = 5000; // Default is 5 seconds (5000). Make this shorter or longer depending on when you actually want the scrolling to start.
 
 //Theming. Default is Blue 1. Make sure to only have one of these on.
